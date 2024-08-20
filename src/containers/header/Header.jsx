b23d6eb3ebafb8e0e0e93c3d15aef3b9b2f9b247
CalvinA-C <calvinalex.currie@gmail.com>
2024-08-20 13:40:31 +1000

    Finishing__Up--header--Mountify

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import mount from "../../assets/mount-graphic.png.png";
+import tv from "../../assets/tv-graphic.png";
+
 import "./header.css";
 
 const Header = () => {
-  return <div className="mountify__header">
-    
-  </div>;
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    // Trigger the transition by setting isLoaded to true after the component mounts
+    setIsLoaded(true);
+  }, []);
+
+  return (
+    <div className="mountify__header section__padding">
+      <div className="mountify__header-contents">
+        <h1>
+          Gold Coast's <br />
+          TV & Soundbar Wall Mounting Service
+        </h1>
+        <p>
+          Experience top-tier TV and soundbar installation with Mountify, Gold
+          Coast's trusted experts. Our lifetime warranty ensures 100%
+          satisfaction, with every job completed to the highest standards. We
+          guarantee a strong frame, premium materials, and precise installation.
+          Enjoy a safer, more stylish home entertainment setup with our
+          commitment to excellence and fast, reliable service.
+        </p>
+        <button>Get Your Free Quote</button>
+      </div>
+      <div className="mountify__header-image">
+        <img
+          className={`mount-graphic ${isLoaded ? "loaded" : ""}`}
+          src={mount}
+          alt="mountify mount graphic"
+        />
+        <img
+          className={`tv-graphic ${isLoaded ? "loaded" : ""}`}
+          src={tv}
+          alt="mountify tv graphic"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
