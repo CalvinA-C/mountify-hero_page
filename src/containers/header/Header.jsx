a38f1c0bd7a0a047395e4995ea4a83b88953b9ea
CalvinA-C <calvinalex.currie@gmail.com>
2024-08-22 12:00:12 +1000

    mountify-mobile-nav_header-complete

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -6,7 +6,16 @@ import "./header.css";
 const Header = () => {
   return (
     <div className="mountify__header">
-
+      <div className="mountify__header--contents">
+        <h1>
+          GOLD COASTS <br />
+        </h1>
+        <h1>TV & Soundbar Mounting</h1>
+      </div>
+      <div className="mountify__header--visuals">
+        <img src={mount} alt="" />
+        <img src={tv} alt="" />
+      </div>
     </div>
   );
 };
