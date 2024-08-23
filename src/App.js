8c4fbe4de6aa64b64ab5123038b1353951d0b243
CalvinA-C <calvinalex.currie@gmail.com>
2024-08-23 11:46:25 +1000

    Starting__Offer--section--mountify-mobile-resolution

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import "./app.css";
 import Navbar from "./components/navbar/Navbar";
 import Header from "./containers/header/Header";
 import Projects from "./components/projects/Projects";
+import Offer from "./containers/offer/Offer";
 
 const App = () => {
   return (
-    <div className="gradient__bg">
-      <Navbar />
-      <Header />
-      <Projects />
-    </div>
+    <>
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+        <Projects />
+      </div>
+      <Offer />
+    </>
   );
 };
 
---
+++ src/containers/offer/Offer.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./offer.css";
+
+const Offer = () => {
+  return (
+    <div className="mountify__offer">
+
+      <div className="mountify__offer-title"></div>
+      
+      <div className="offer">
+        <div className="offer-title"></div>
+        <div className="offer-text"></div>
+      </div>
+    
+      <div className="offer">
+        <div className="offer-title"></div>
+        <div className="offer-text"></div>
+      </div>
+
+ 
+    </div>
+  );
+};
+
+export default Offer;
